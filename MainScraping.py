693078005d1a09927cd58b09d62a48c95b41d35b
Renan Redel <renan.augusto1133@icloud.com>
2020-05-11 20:54:07 -0300

    Update MainScraping
    Está pegando os dados da página e abrindo a segunda página de dados.

--- MainScraping.py
+++ MainScraping.py
@@ -3,6 +3,28 @@ import urllib.request
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
+
+def scrap(driverPage, url):
+    soup = BeautifulSoup(driverPage, 'html.parser')
+
+    indice = 0
+    for result in soup.findAll('table', {'class': 'tableDados'}):
+        for line in result.findAll('td'):
+            indice = indice + 1
+            print(indice)
+            print(line.text.ljust(10))
+            if indice / 6 == 50:
+                nextpage(url)
+
+
+
+def nextpage(url):
+    driver2 = webdriver.Firefox()
+    driver2.get(url)
+    driver2.find_element_by_xpath("//*[@id='menuPaginacao']/li[5]/a").click()
+
+
+
 url = "https://transparencia.joinville.sc.gov.br/?p=5&inicio=01/01/2020&fim=31/12/2020"
 page = urllib.request.urlopen(url)
 
@@ -10,38 +32,35 @@ driver = webdriver.Firefox()
 
 driver.get(url)
 
-
 #### SELECIONA A UNIDADE
 
 unidadeSelect = driver.find_element_by_id("id_entidade")
 unidadeSelect.click()
-#unidadeSelect.send_keys("Prefeitura")
-unidadeEscolhida = driver.find_element_by_xpath("//*[@id='id_entidade']/option[15]").click()
+# unidadeSelect.send_keys("Prefeitura")
+driver.find_element_by_xpath("//*[@id='id_entidade']/option[15]").click()
 
 #### SELECIONA O MES
-mesSelect = driver.find_element_by_id("nr_mes").click()
-mesEscolhido = driver.find_element_by_xpath("//*[@id='nr_mes']/option[1]").click()
+driver.find_element_by_id("nr_mes").click()
+driver.find_element_by_xpath("//*[@id='nr_mes']/option[1]").click()
 
 #### SELECIONA O ANO
-anoSelect = driver.find_element_by_id("nr_ano").click()
-anoEscolhido = driver.find_element_by_xpath("//*[@id='nr_ano']/option[2]").click()
+driver.find_element_by_id("nr_ano").click()
+driver.find_element_by_xpath("//*[@id='nr_ano']/option[2]").click()
 
 #### SELECIONA A SITUAÇÃO
-situacaoSelect = driver.find_element_by_id("id_situacao").click()
-situacaoEscolhida = driver.find_element_by_xpath("//*[@id='id_situacao']/option[19]").click()
+driver.find_element_by_id("id_situacao").click()
+driver.find_element_by_xpath("//*[@id='id_situacao']/option[19]").click()
 
 #### SELECIONA O VINCULO
-vinculoSelect = driver.find_element_by_id("iVinculoFiltroPeloCampo").click()
-vinculoEscolhido = driver.find_element_by_xpath("//*[@id='iVinculoFiltroPeloCampo']/option[6]").click()
+driver.find_element_by_id("iVinculoFiltroPeloCampo").click()
+driver.find_element_by_xpath("//*[@id='iVinculoFiltroPeloCampo']/option[6]").click()
 
 #### DIGITA O CARGO
-cargo = driver.find_element_by_id("ds_cargo").send_keys("EDUCADOR")
+driver.find_element_by_id("ds_cargo").send_keys("EDUCADOR")
 
 #### CONSULTAR
 driver.find_element_by_xpath("/html/body/form/div[4]/table[1]/tbody/tr[6]/td[2]/input").click()
 
-soup = BeautifulSoup(driver.page_source)
+# soup = BeautifulSoup(driver.page_source)
 
-for result in soup.findAll('table', {'class' : 'tableDados'}):
-    for line in result.findAll('td'):
-        print(line.text.ljust(10))
+scrap(driver.page_source, driver.current_url)
