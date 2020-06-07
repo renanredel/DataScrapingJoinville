d5a00ea29e069207855059b04e144a302850330a
Renan Redel <renan.augusto1133@icloud.com>
2020-06-06 21:48:00 -0300

    Novos Gráficos

--- plot/graphs.py
+++ plot/graphs.py
@@ -1,6 +1,7 @@
 import matplotlib
 import pandas as pd
 from matplotlib import pyplot as plt
+import numpy as np
 import tornado
 
 matplotlib.use("WebAgg")
@@ -8,13 +9,13 @@ matplotlib.use("WebAgg")
 
 class PlotClass:
     def plot1(self):
-        data = pd.read_csv("/home/renanredel/PycharmProjects/DataScrapingJoinville/resultado/valores.csv",
+        data = pd.read_csv("/home/renanredel/PycharmProjects/DataScrapingJoinville/resultado/professores.csv",
                            sep=",")
         ### REMOVE A PRIMEIRA COLUNA DO CSV ###
         data = data.drop(data.columns[0], axis=1)
-        # print(data.loc[data['liquido'] == 0])
         print(data)
         data.drop(data[data['liquido'] == 0].index, inplace=True)
+        data.drop(data[data['anos_trabalhados'] >= 30].index, inplace=True)
         data.reset_index(drop=True, inplace=True)
         print(data)
         mens = data[data.genero == "M"]
@@ -29,10 +30,61 @@ class PlotClass:
         print("mulheres")
         print(womans)
 
-        plt.plot(womans.liquido, womans.anos_trabalhados)
-        plt.plot(mens.liquido, mens.anos_trabalhados)
+        plt.plot(womans.anos_trabalhados, womans.liquido, label='Mulheres')
+        plt.plot(mens.anos_trabalhados, mens.liquido, label='Homens')
         plt.title("Salario conforme tempo trabalhado")
-        plt.xlabel("Salario")
-        plt.ylabel("Anos de serviço")
-        plt.legend("Mulheres", "Homens")
+        plt.xlabel("Anos de serviço")
+        plt.ylabel("Salário")
+        plt.legend()
+
         plt.show()
+
+
+    def plot2(self):
+        data = pd.read_csv("/home/renanredel/PycharmProjects/DataScrapingJoinville/resultado/professores.csv",
+                           sep=",")
+        data = data.drop(data.columns[0], axis=1)
+        data.drop(data[data['liquido'] == 0].index, inplace=True)
+        data.drop(data[data['anos_trabalhados'] >= 30].index, inplace=True)
+        labels = 'Homens', 'Mulheres'
+        male = data[data.genero == "M"]
+        woman = data[data.genero == "F"]
+        sizemale = (len(male) / 12)
+        sizewoman = (len(woman) / 12)
+        manostrabalhados = woman.groupby(['anos_trabalhados']).size()
+        hanostrabalhados = male.groupby(['anos_trabalhados']).size()
+
+
+        manostrabalhados = round((manostrabalhados/12)+ .5).sort_values()
+        hanostrabalhados = round((hanostrabalhados/12)+ .5).sort_values()
+
+        fig, ax = plt.subplots(2,2)
+
+        size = 0.3
+        vals = ([[sizemale], [sizewoman]])
+
+        cmap = plt.get_cmap("tab20c")
+        outer_colors = cmap(np.arange(3) * 4)
+        inner_colors = cmap(np.array([1, 2, 5, 6, 9, 10]))
+
+        ax[0,0].pie(vals, radius=1.3, colors=outer_colors,
+               wedgeprops=dict(width=size, edgecolor='w'), labels=labels, autopct='%1.1f%%')
+
+        ax[0,1].pie(manostrabalhados, radius=1.5 - size, colors=inner_colors,
+                    labels=manostrabalhados.index.tolist(), autopct='%1.0f%%')
+
+        ax[1,1].pie(hanostrabalhados, radius=1.5 - size, colors=inner_colors,
+               labels=hanostrabalhados.index.tolist(), autopct='%1.0f%%')
+
+        ax[1,0].remove()
+
+        ax[0,0].set(aspect="equal", title='Porcentagem Homens e Mulheres')
+        ax[0,1].set(aspect="equal", title='Tempo de serviço - Mulheres')
+        ax[1,1].set(aspect="equal", title='Tempo de serviço - Homens')
+
+        plt.show()
+
+
+
+
+
